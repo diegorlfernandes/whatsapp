62dcf84aea6048198d35a3626173828c1d840a86
Diego Rodrigues de Lima Fernandes <diego.fernandes@ms.senac.br>
2019-01-03 15:38:07 -0300

    ajuste de layout

---
+++ index.js
@@ -0,0 +1,19 @@
+$(document).ready(function($){
+
+    $("#celular").mask("(99) 99999-9999");
+    $('#celular').blur(function (event) {
+      if ($(this).val().length == 15) { // Celular com 9 dígitos + 2 dígitos DDD e 4 da máscara
+        $('#celular').mask('(99) 99999-9999');
+      } else {
+        $('#celular').mask('(99) 9999-99999');
+      }
+    });
+
+
+    $("#btn").click(function () {
+      var phone = $("#celular").val();
+      window.location.href = "https://api.whatsapp.com/send?phone=55" + phone + "&text=sua%20mensagem";
+
+    });
+
+  });
